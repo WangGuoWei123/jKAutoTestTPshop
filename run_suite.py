f7602c4c70de89d98be193a01684fd9c8815eb58
wangguowei <wangguowei0126@163.com>
2024-03-25 01:24:54 +0800

    修改生成报告文件名

--- run_suite.py
+++ run_suite.py
@@ -2,6 +2,7 @@ import unittest
 
 import time
 
+from app import BASE_DIR
 from case.tp_case import LoginTest, OrderTest
 from tools.HTMLTestRunner import HTMLTestRunner
 
@@ -11,7 +12,7 @@ suite.addTest(unittest.makeSuite(LoginTest))
 suite.addTest(unittest.makeSuite(OrderTest))
 # unittest.TextTestRunner().run(suite)
 # with open("./report/" + time.strftime("%Y%m%d %H%M%S") +  ".html","wb") as f:
-with open("./report/report.html","wb") as f:
+with open(BASE_DIR+"./report/report.html","wb") as f:
     # 使用 HTMLTestRunner 要运行测试套件，将结果写入文件流
     runner = HTMLTestRunner(f,title="我的测试报告",description="测试TPShop登录接口")
     runner.run(suite)
