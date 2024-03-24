6dea9f0e0ee4209b45738fc9ad7b1aa7a1f9481d
wangguowei <wangguowei0126@163.com>
2024-03-25 00:53:59 +0800

    修改生成报告文件名

--- run_suite.py
+++ run_suite.py
@@ -10,7 +10,8 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(LoginTest))
 suite.addTest(unittest.makeSuite(OrderTest))
 # unittest.TextTestRunner().run(suite)
-with open("./report/" + time.strftime("%Y%m%d %H%M%S") +  ".html","wb") as f:
+# with open("./report/" + time.strftime("%Y%m%d %H%M%S") +  ".html","wb") as f:
+with open("./report/report.html") as f:
     # 使用 HTMLTestRunner 要运行测试套件，将结果写入文件流
     runner = HTMLTestRunner(f,title="我的测试报告",description="测试TPShop登录接口")
     runner.run(suite)
